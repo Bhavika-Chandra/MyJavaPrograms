cf99d3b74e4c4b9b90c3cdbd894abfab9218e55b
Bhavika Chandra <113355880+Bhavika-Chandra@users.noreply.github.com>
2023-04-24 11:28:01 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -1,10 +1,7 @@
-//import java.util.*;
 class factorial{  
  public static void main(String args[]){  
   int i,fact=1; 
-  int n=5; 
-  //System.out.println("Enter a number:");
-  //int n = scn.nextInt();  
+  int n=5;   
   for(i=1;i<=n;i++){    
       fact=fact*i;    
   }    
