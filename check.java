6ba74c9ee731550ac6e6350bbb4451a7861f4312
Bhavika Chandra <113355880+Bhavika-Chandra@users.noreply.github.com>
2023-05-06 19:21:27 +0530

    Add files via upload
    In this program I have implemented try-catch block.

---
+++ check.java
@@ -0,0 +1,11 @@
+public class check {
+ public static void main(String args[]){
+    try{
+        System.out.println(4/0);
+	  System.out.println("Programming");
+    }
+    catch(Exception e){
+	  System.out.println("Error in try block!");
+    }
+ }  
+}
