6298d8e332079c00fdf01c25fb2323eb8a3f9f23
Bhavika Chandra <113355880+Bhavika-Chandra@users.noreply.github.com>
2023-05-18 11:14:42 +0530

    Update queue.java

--- queue.java
+++ queue.java
@@ -6,7 +6,7 @@ Queue <Integer> numbers=new LinkedList<>();
 numbers.offer(1);
 numbers.offer(2);
 numbers.offer(3);
-Sytem.out.println("Queue:"+numbers);
+System.out.println("Queue:"+numbers);
 int removedNumber=numbers.poll();
 System.out.println("Removed element:"+removedNumber);
 System.out.println("Queue after deletion:"+numbers);
