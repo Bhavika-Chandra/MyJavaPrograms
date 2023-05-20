e2190a8720a2fe934b6b5a5e3b795774af170ece
Bhavika Chandra <113355880+Bhavika-Chandra@users.noreply.github.com>
2023-05-20 11:47:27 +0530

    Update AnnotationDemo.java

--- AnnotationDemo.java
+++ AnnotationDemo.java
@@ -1,3 +1,5 @@
+//import java.util.List;
+//import java.util.ArrayList;
 import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
@@ -12,7 +14,8 @@ public class AnnotationDemo {
     }
     @SuppressWarnings("unchecked")
     public static void suppressWarningExample() {
-        java.util.List myList = new java.util.ArrayList();
+        java.util.List myList = new java.util.ArrayList(); //By importing the two commented classes on the top we can also write it as:
+                                                            // List<String> myList = new ArrayList<>(); 
         myList.add("Item");
     }
     @MyAnnotation("Sample Annotation")
